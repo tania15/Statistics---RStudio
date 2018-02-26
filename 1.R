862a80e1ba472edff51b78d7c7c305a5a59642f1
Tania Grudeva <tpgrudeva@uni-sofia.bg>
2018-02-26 21:05:16 +0200

    Add

---
+++ 1.R
@@ -0,0 +1,18 @@
+##
+data = c(1,2,3,4,5)
+data1 <- c(6,7,8,9) 
+data
+data1
+
+# ôóíêöèè
+mean(data)     # [1] 3
+mean(data1)    # [1] 7.5
+
+median(data)   # [1] 3
+median(data1)  # [1] 7.5
+
+var(data)      # [1] 2.5
+var(data1)     # [1] 1.666667
+
+
+
